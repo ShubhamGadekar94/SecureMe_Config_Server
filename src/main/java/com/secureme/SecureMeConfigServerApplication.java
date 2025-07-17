c7743bd0485f67af0e28f0ac5752d8c898330a3f
ShubhamTheSavior <shubham.gadekar99@gmail.com>
2025-07-17 15:29:53 +0530

    code updated

--- src/main/java/com/secureme/SecureMeConfigServerApplication.java
+++ src/main/java/com/secureme/SecureMeConfigServerApplication.java
@@ -2,7 +2,11 @@ package com.secureme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -12,4 +16,8 @@ public class SecureMeConfigServerApplication {
 		SpringApplication.run(SecureMeConfigServerApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder, SslBundles sslBundles) {
+		return restTemplateBuilder.setSslBundle(sslBundles.getBundle("secureme-config")).build();
+	}
 }
